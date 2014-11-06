417aeacf592ce66c7650628dd4d4552e49b52f95
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 10:50:01 +0100

    Cleanup tests

--- tests/test_sale_margin_waiting.py
+++ tests/test_sale_margin_waiting.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
-#This file is part sale_margin_waiting module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part sale_margin_waiting module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
 
 class SaleMarginWaitingTestCase(unittest.TestCase):
-    '''
-    Test Sale Margin Waiting module.
-    '''
+    'Test Sale Margin Waiting module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_margin_waiting')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -35,6 +23,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleMarginWaitingTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
